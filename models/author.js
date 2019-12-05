441ebaa7d15eff0a6f7f6db6c345e7cdff66cfc2
turgaysozen <turgaysozen@hotmail.com>
2019-12-06 00:31:49 +0300

    View-Delete-Update Routes Changed

--- models/author.js
+++ models/author.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose');
+const Book = require('./book')
 
 const authorShema = new mongoose.Schema({
-name:{
-    type:String,
-    required: true
-}
+    name: {
+        type: String,
+        required: true
+    }
+});
+authorShema.pre('remove', function (next) {
+    Book.find({ author: this.id }, (err, books) => {
+        if (err) {
+            next(err);
+        }
+        else if(books.length > 0){
+            next(new Error('This Author has books'));
+        }    
+        else next();
+    });
 });
 
 module.exports = mongoose.model('Author', authorShema);
--- routes/authors.js
+++ routes/authors.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const Author = require('../models/author');
+const Book = require('../models/book');
+
 
 //All authors route
 router.get('/', async (req, res) => {
-    let searchObject = {}
+    let searchObject;
     if (req.query.name != null && req.query.name !== '') {
         searchObject.name = new RegExp(req.query.name, 'i');
     }
     try {
         const authors = await Author.find(searchObject);
+        
         res.render('authors/index', {
             authors: authors,
-            searchObject: req.query
+            searchObject: req.query,
         });
-    } catch{
+    } catch (err){
+        console.log(err)
         res.redirect('/');
     }
-   // res.render('authors/index');
+    // res.render('authors/index');
 });
 
 // New author route
@@ -30,26 +34,77 @@ router.post('/', async (req, res) => {
     const author = new Author({
         name: req.body.name,
     });
-    
+
     try {
-        const newAuthor = await author.save();
-        res.redirect(`authors`);
+        await author.save();
+        //res.redirect(`/authors/${author.id}`);
+        res.redirect('/authors');
+
     } catch  {
         res.render('authors/new', {
             author: author,
-            errorMessage: author +' Error Creating Author'
+            errorMessage: author + ' Error Creating Author'
         });
     }
+});
 
+router.get('/:id', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id);
+        const books = await Book.find({ author: author.id }).limit(10).exec();
+        res.render('authors/view', {
+            author: author,
+            booksByAuthor: books,
+        });
+    } catch {
+        res.redirect('/');
+    }
+});
 
-    // author.save((err, newAuthor) => {
-    //     if (err) {
-    //         res.render('authors/new', {
-    //             author: author,
-    //             errorMassage: 'Error Creating Author'
-    //         });
-    //     } else res.redirect(`authors`);
-    // });
+router.get('/:id/edit', async (req, res) => {
+    const author = await Author.findById(req.params.id);
+    try {
+        res.render('authors/edit', {
+            author: author,
+        });
+    } catch  {
+        res.redirect('/authors');
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    let author;
+    try {
+        author = await Author.findById(req.params.id);
+        author.name = req.body.name;
+        await author.save();
+        res.redirect(`/authors/${author.id}`);
+    } catch  {
+        if (author == null) {
+            res.redirect('/');
+        }
+        else {
+            res.render('authors/edit', {
+                author: author,
+                errorMessage: 'Error Updating Author',
+            });
+        }
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id);
+        if (author != null) {
+            author.remove();
+            res.redirect('/authors');
+        }
+    } catch  {
+        if(author != null){
+            res.redirect(`/authors/${author.id}`);
+        }
+        else res.redirect('/authors');
+    }
 });
 
 module.exports = router;
